e43fc122bad7faaee32d09d6251f20825a7cb6a5
sebastiancwk <sebastiancwking@gmail.com>
2023-07-08 17:27:05 +1000

    updated styles and copy

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,9 +3,15 @@ import Image from 'next/image'
 export default function Home() {
   return (
     <main>
-      hello
-      <br/>
-      I&apos;m Seb
+      <div style={{ height: '100vh' }} className="flex border justify-center items-center">
+        <div className="border p-5 rounded">
+          <h1>Hello</h1>
+          <p>I&apos;m Seb</p>
+          <p>Currently at GoodHuman</p>
+          <p><a href="https://www.linkedin.com/in/sebastian-king-15011b171/">LinkedIn</a></p>
+          <p><a href="https://github.com/sebastiancwk">GitHub</a></p>
+        </div>
+      </div>
     </main>
   )
 }

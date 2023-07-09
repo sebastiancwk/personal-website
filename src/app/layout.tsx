c20927dfda7d129089ce3b72103fe3496d5f1336
sebastiancwk <sebastiancwking@gmail.com>
2023-07-09 20:42:27 +1000

    updated styles

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,22 +1,42 @@
-import './globals.css'
-import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
+import "./globals.css";
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import localFont from "next/font/local";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
+
+const clashDisplay = localFont({
+  src: [
+    {
+      path: "../../public/fonts/clash-display/ClashDisplay-Semibold.ttf",
+      weight: "600",
+    },
+  ],
+  variable: "--font-clash-display",
+});
+
+const archivo = localFont({
+  src: [
+    { path: "../../public/fonts/archivo/Archivo-Regular.ttf", weight: "500" },
+  ],
+  variable: '--font-archivo'
+});
 
 export const metadata: Metadata = {
-  title: 'Seb',
-  description: 'Sebastian King',
-}
+  title: "Seb",
+  description: "Sebastian King",
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={`${clashDisplay.variable} ${archivo.variable} ${archivo.className}`}>
+        {children}
+      </body>
     </html>
-  )
+  );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -12,6 +12,10 @@ module.exports = {
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
+      fontFamily: {
+        heading: ['var(--font-clash-display)'],
+        sans: ['var(--font-archivo)']
+      }
     },
   },
   plugins: [],
